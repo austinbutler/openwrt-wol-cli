9e81b9e1c5fe3eed4d2b8324ad090723e37e033e
Austin Butler <austinabutler@gmail.com>
2021-09-19 11:08:13 -0700

    Update for 21.02

--- wol.sh
+++ wol.sh
@@ -1,21 +1,19 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 # Get device name from parameter
 HOST="$1"
 
-# Get all available hosts from hosts file
-HOSTS=$(uci show dhcp | grep name | cut -d"'" -f2 | sed ':a;N;$!ba;s/\n/, /g')
-
 # Test if a host was supplied, exit with error if not
 if [ -z "$HOST" ]; then
-  echo "No valid host specified, valid options: $HOSTS"
-  echo "You may also use a partial name as long as it produces a single match"
-  exit 1
+	HOSTS=$(uci show dhcp | grep name | cut -d"'" -f2 | sed ':a;N;$!ba;s/\n/, /g')
+	echo "No valid host specified, valid options: $HOSTS"
+	echo "You may also use a partial name as long as it produces a single match"
+	exit 1
 fi
 
 # Get IP from hostsfile based on hostname
 MAC=$(uci get dhcp."${HOST}".mac)
 
-LAN_IF=$(uci get network.lan.ifname)
+LAN_IF=$(uci get network.lan.device)
 
 etherwake -D -i "$LAN_IF" "$MAC"
